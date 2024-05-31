65df9a2bdb61f7a4733f8889db563e642b622301
Augustin Montredon <augustinmtrd@gmail.com>
2024-05-30 20:59:26 -0400

    Fonction decryptage brute force en vérifiant sur fichier mots_pendu.txt

--- input_reading.py
+++ input_reading.py
@@ -14,6 +14,11 @@ def lecture_fichier():
     return contenu
 
 
+def mot_dans_fichier(mot):
+    mots = lecture_fichier().splitlines()
+    return mot in mots
+
+
 def lecture_entree():
     return input('Quel message souhaitez-vous décrypter ?')
 
@@ -27,7 +32,6 @@ def ecriture(file_name,contenu):
 if __name__ == '__main__':
     choix = 0
     file_name = lecture_fichier()
-    key = 1
     while choix != 1 or choix != 2:
         choix = int(input('Souhaitez-vous entrer votre propre message ou en charger un ? \n'
               '1 - Message personnalisé \n'
@@ -36,7 +40,13 @@ if __name__ == '__main__':
 
         if choix == 1:
             message = lecture_entree()
-            message_decrypt = decrypt_message.decrypter(message, key)
+            file_name = lecture_fichier()
+            choix_key = input('Souhaitez-vous fournir une clé de décryptage ? ')
+            try:
+                message_decrypt, key = decrypt_message.decrypter(message, int(choix_key))
+            except ValueError:
+                print('Aucune clé détectée')
+                message_decrypt, key = decrypt_message.decrypter(message)
             print('Le message décrypté est : ', message_decrypt, ' et a été crypté avec une clé = ', key)
 
         elif choix == 2:
