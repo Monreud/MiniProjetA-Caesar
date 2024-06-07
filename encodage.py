aa0620456f680c9815c00550fa05da40039232eb
Baptiste-corn <61588561+Baptiste-corn@users.noreply.github.com>
2024-06-07 19:15:21 -0400

    Encodage d'une phrase reçue en paramètre

--- encodage.py
+++ encodage.py
@@ -5,11 +5,13 @@ Encodage César
 import string
 from random import randint
 from random import choice
+
 # test git
 def encodage(mot_a_encoder, cle_decriptage):
     mot_crypte = ""
     alphabet = string.ascii_lowercase  # On recupere l'alphabet
     alphabet_crypte = ""
+    mot_a_encoder = mot_a_encoder.lower() # On convertit les majuscules de l'utilisateur en minuscule
     ### Test boucle sur tout l'alphabet
     for i in range(len(alphabet)):
         alphabet_crypte += alphabet[(i + cle_decriptage) % 26]
@@ -24,13 +26,11 @@ def encodage(mot_a_encoder, cle_decriptage):
     return mot_crypte
 
 def encodage_fichier ():
+
     fichier_utilisateur = "mots_pendu.txt"
     fichier = open(fichier_utilisateur, "r", encoding="utf-8")
     fichier_encode = open("mots_encode.txt", "w", encoding="utf-8")
-
     banque_de_mots = fichier.read().splitlines()
-
-
 
     for i in range(len(banque_de_mots)):
         cle_aleatoire = randint(1, 25)  # Une clé à 0 n'encode pas le mot
@@ -38,5 +38,18 @@ def encodage_fichier ():
         fichier_encode.write(f"{banque_de_mots_encode}\n")
     return fichier_encode
 
+def encodage_phrase(phrase_a_encoder, cle_decriptage):
+    phrase_encode =""
+    liste_cript = phrase_a_encoder.split(" ")
+    print(liste_cript)
+
+    for mot in liste_cript:  # On parcourt chaque mot de la liste
+        phrase_encode += encodage(mot, cle_decriptage)  # On concatene chaque mot crypte dans la chaine de caractere
+        phrase_encode += " " # Espace
+
+    return phrase_encode
+
+
 encodage("test", -2)
 encodage_fichier()
+encodage_phrase("Je suis message", 0)
